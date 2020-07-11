e78d1b52d1ef436720ea2fc1dd827a7ca5904fb0
Ryan Sprott <ryan_sprott@moma.org>
2020-07-11 00:49:20 -0400

    Yaws!

--- tellopy/examples/turn_to_yaw_degree.py
+++ tellopy/examples/turn_to_yaw_degree.py
@@ -28,42 +28,48 @@ def test():
         drone.takeoff()
         sleep(2)
 
-        target_yaw = 180.0
+        target_yaw = 0.0
+        yaws = [0.0, 45.0, 90.0, 135.0, -179.0, -135.0, -90.0, -45.0, 0.0]
         while True:
             current_yaw = drone.log_data.imu.yaw
 
-            current_neg = current_yaw < 0.0
-            target_neg  = target_yaw < 0.0
-            adj_current = 360.0 + current_yaw if current_neg else current_yaw
-            adj_target  = 360.0 + target_yaw if target_neg else target_yaw
+            adj_current = 360.0 + current_yaw if current_yaw < 0.0 else current_yaw
+            adj_target  = 360.0 + target_yaw if target_yaw < 0.0 else target_yaw
 
-            delta = adj_target - adj_current
-            abs_delta = abs(delta)
+            delta = round(adj_target - adj_current, 2)
 
-            if (abs_delta >= 180.0 and delta > 0.0):
+            if (abs(delta) >= 180.0 and delta > 0.0):
                 delta -= 360.0
-            elif (abs_delta >= 180.0 and delta < 0.0):
+            elif (abs(delta) >= 180.0 and delta < 0.0):
                 delta += 360.0
 
-            print(str(delta) + " to " + str(adj_target))
+            direction = ""
 
             if (delta > 10.0):
-                print("fast CW")
-                drone.clockwise(70)
+                direction = "fast CW "
+                drone.clockwise(60)
             elif (delta > 0.0):
-                print("slow CW")
+                direction = "slow CW "
                 drone.clockwise(10)
             elif (delta < -10.0):
-                print("fast CCW")
-                drone.counter_clockwise(70)
+                direction = "fast CCW "
+                drone.counter_clockwise(60)
             elif (delta < 0.0):
-                print("slow CCW")
+                direction = "slow CCW "
                 drone.counter_clockwise(10)
 
-            if (math.isclose(0.0, delta, abs_tol=0.5)):
+            print(direction + str(delta) + " from " + str(adj_current) + " to " + str(adj_target))
+
+            if (math.isclose(0.0, delta, abs_tol=1.0)):
                 # close enough for government work!
-                drone.clockwise(0)
                 print("done!")
+                drone.clockwise(0)
+                sleep(1) # stabilize
+                drone.take_picture()
+                sleep(4)
+                target_yaw = yaws.pop()
+
+            if (len(yaws) == 0):
                 break
 
             sleep(0.1)
